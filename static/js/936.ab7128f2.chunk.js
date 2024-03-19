8b580a9205a349f49eed1b00d8ed72ecbec48397
Marynryn <132609093+Marynryn@users.noreply.github.com>
2024-03-19 18:37:26 +0000

    Deploying to gh-pages from @ Marynryn/phonebook@45543de69827c020c29b9664c0a5492c34f12181 🚀

---
+++ static/js/936.ab7128f2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[936],{4936:function(e,t,o){o.r(t),o.d(t,{default:function(){return a}});var n="Home_title__L087v",r="Home_container__w+u63",c=o(184);function a(){return(0,c.jsx)("div",{className:r,children:(0,c.jsxs)("h1",{className:n,children:["Welcome to your phonebook"," "]})})}}}]);
+//# sourceMappingURL=936.ab7128f2.chunk.js.map
